393f7391a6eaedacf8788bacd79bd90e49e1260f
Qinisela.Molefe <Qinisela.Molefe@investec.co.za>
2018-11-01 17:57:28 +0200

    Adding unit testing project

---
+++ ABSAProject.Console/RobotGridState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ABSAProject.Console
+{
+    public class RobotGridState
+    {
+        public RobotGridState()
+        {
+            XPoint = 0;
+            YPoint = 0;
+            PreviuosStep = null;
+            StepCount = 0;
+            RightTurnCount = 0;
+            UniqueStepsTaken = new Dictionary<string, string>();
+        }
+
+        public int StepCount { get; set; }
+        public string PreviuosStep { get; set; }
+        public int XPoint { get; set; }
+        public int YPoint { get; set; }
+        public int RightTurnCount { get; set; }
+        public Dictionary<string, string> UniqueStepsTaken { get; set; }
+    }
+}
